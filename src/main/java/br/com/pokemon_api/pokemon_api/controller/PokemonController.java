90b0552b86b6a2798109019bf1b03b780a4d643b
Murillo Morgado <mumorgadok@hotmail.com>
2024-05-16 20:27:34 -0300

    controller pokemon

--- src/main/java/br/com/pokemon_api/pokemon_api/controller/PokemonController.java
+++ src/main/java/br/com/pokemon_api/pokemon_api/controller/PokemonController.java
@@ -4,24 +4,52 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pokemon_api.pokemon_api.model.Pokemon;
 import br.com.pokemon_api.pokemon_api.service.PokemonService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/pokemon")
 public class PokemonController {
-  
+
   @Autowired
   private PokemonService pokemonService;
 
-
-  @Autowired
-  public ResponseEntity<List<Pokemon>> listarPokemon(){
+  @GetMapping
+  public ResponseEntity<List<Pokemon>> listarPokemon() {
     List<Pokemon> listarPokemon = pokemonService.listarPokemon();
     return ResponseEntity.ok().body(listarPokemon);
   }
-  
+
+  @GetMapping(value = "/{idPokemon}")
+  public ResponseEntity<Pokemon> buscarPokemon(@PathVariable Long idPokemon) {
+    Pokemon buscarPokemon = pokemonService.buscarPokemon(idPokemon);
+    return ResponseEntity.ok().body(buscarPokemon);
+  }
+
+  @PostMapping
+  public ResponseEntity<Long> criarPokemon(@RequestBody Pokemon pokemon) {
+    Long idPokemon = pokemonService.criarPokemon(pokemon).getIdPokemon();
+    return ResponseEntity.ok().body(idPokemon);
+  }
+
+  @PutMapping(value = "/{idPokemon}")
+  public ResponseEntity<Pokemon> atualizarPokemon(@PathVariable Long idPokemon, Pokemon pokemon) {
+    pokemonService.atualizarPokemon(idPokemon, pokemon);
+    return ResponseEntity.ok().build();
+  }
+
+  @DeleteMapping(value = "/{idPokemon}")
+  public ResponseEntity<Pokemon> deletarPokemon(@PathVariable Long idPokemon) {
+    pokemonService.deletarPokemon(idPokemon);
+    return ResponseEntity.ok().build();
+  }
 }
